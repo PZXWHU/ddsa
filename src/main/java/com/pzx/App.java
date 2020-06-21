ff904b0585883b2218d7c21af8796019c7f93ec8
PZXGit <2462517990@qq.com>
2020-06-21 15:59:44 +0800

    trap water in plane

--- src/main/java/com/pzx/App.java
+++ src/main/java/com/pzx/App.java
@@ -10,7 +10,8 @@ public class App
 {
     public static void main( String[] args )
     {
-
+        boolean[] t  = new boolean[10];
+        System.out.println(t[0]);
 
     }
 
--- src/main/java/com/pzx/algorithm/TrapRain.java
+++ src/main/java/com/pzx/algorithm/TrapRain.java
@@ -1,7 +1,6 @@
 package com.pzx.algorithm;
 
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.*;
 
 /**
  * 接雨水问题
@@ -14,7 +13,7 @@ public class TrapRain {
      * 1.求出数组两端的初始peak（按顺序下一个元素小于当前元素，即可认为是peak）
      * 2.若两个初始peak位置重合，则返回0，当前地形处于凸出状态
      * 3.从左边的初始peak向右遍历（直到右边的peak），如果遇到height大于当前左peak，则计算当前左peak和当前位置之间的雨水量。并将当前左peak的位置移动到当前位置
-     * 4.从右边初始peak向左遍历（直到左边的peak），如果遇到height大于当前右peak，则计算当前右peak和当前位置的雨水量，并将当前右peak的位置移动到当前位置。
+     * 4.从右边初始peak向左遍历（直到左边的peak），如果遇到height大于当前右peak，则计算当前右peak和当前位置的雨水量，并将当前右peak的位置移动到当前位置。（此步和上一步有重复扫描的元素）
      * 5.将计算的雨水量相加，返回结果
      *
      * @param height
@@ -134,5 +133,65 @@ public class TrapRain {
         return result;
     }
 
+    /**
+     * 二维平面接水问题
+     * 算法过程：
+     * 1.创建对应二维数组，标志某一单元是否已经检查过了
+     * 2.创建优先队列，小顶堆
+     * 3.将二维数据边界的单元全部入队列，以（x，y，height）为队列储存单元。并且将其对应的检查位设置为true。
+     * 4.如果队列不为空，执行出队列操作。对获取的单元向上下左右扩展，如果不超出边界而且未检查过，则判断扩展单元的height是否小于当前单元，小于则将height差值加到结果中。
+     * 5.最后将扩展单元的的检查位设置位true。
+     * 6.当队列为空时，则输出结果。
+     *
+     * 算法解析：
+     * 因为使用了优先队列，所以height值小的单元将首先出队列，进行扩展。
+     * 当扩展单元的高度小于当前出队列单元时，扩展单元的周围必被高度值大于等于当前单元的单元所包围，所以可以直接为扩展单元补充水量：当前单元-扩展单元
+     * 扩展单元高度小于当前单元时，其必然是可以积水的。假设其不能积水，则必有一条高度值均小于当前单元的流出边界的单元路径。但是边界上的元素一开始就入队列，高度值较小的将先出队列进行扩展。
+     * 因为此条路径的高度值都小于当前单元，所以这些单元必将当前单元先出队列进行扩展。所以假设错误。
+     * 参考：https://www.cnblogs.com/xym4869/p/12595864.html
+     * @param height
+     * @return
+     */
+    private static int trap4(int[][] height){
+        int n = height.length;
+        int m = height[0].length;
 
+        Queue<int[]> priorityQueue = new PriorityQueue<>((o1, o2)->Integer.compare(o1[2],o2[2]));
+        boolean[][] isChecked = new boolean[n][m];
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(i==0 || i==n-1 || j==0 || j==m-1){
+                    priorityQueue.offer(new int[]{i, j, height[i][j]});
+                    isChecked[i][j] = true;
+                }
+            }
+        }
+
+        int result = 0;
+        int[] bias = new int[]{1,-1,0,0};
+        while (!priorityQueue.isEmpty()){
+            int[] poll = priorityQueue.poll();
+            for(int i=0;i<4;i++){
+                int newX = poll[0] + bias[i];
+                int newY = poll[1] + bias[3-i];
+                if(newX>=0 && newX<n && newY>=0 && newY<m && !isChecked[newX][newY]){
+                    if(height[newX][newY] < poll[2]){
+                        result += poll[2] - height[newX][newY];
+                        priorityQueue.offer(new int[]{newX,newY,poll[2]});
+                    }else {
+                        priorityQueue.offer(new int[]{newX,newY,height[newX][newY]});
+                    }
+                    isChecked[newX][newY] = true;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] height = new int[][]{{1,4,3,1,3,2},{3,2,1,3,2,4},{2,3,3,2,3,1}};
+        System.out.println(trap4(height));
+    }
 }
