0ad88e00c4f8b0ede71c1d8c7fdc64c4454d286e
PZXGit <2462517990@qq.com>
2020-06-22 22:28:36 +0800

    BinaryTreeIterativelyTraversal

---
+++ src/main/java/com/pzx/ddsa/BinaryTreeIterativelyTraversal.java
@@ -0,0 +1,116 @@
+package com.pzx.ddsa;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 二叉树非递归遍历
+ */
+public class BinaryTreeIterativelyTraversal {
+
+    private  static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+     }
+
+    /**
+     * 前序遍历
+     * 算法流程：
+     *  当前节点不为空或者栈不为空时，进行以下循环
+     * 1.当前节点不为空，则访问该节点，并入栈。将当前节点赋值为当前节点的左孩子。
+     * 2.当前节点为空，则进行出栈操作。将当前节点赋值为出栈元素的右孩子。
+     * @param root
+     * @return
+     */
+    private static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode node = root;
+
+        while(node!=null||!stack.isEmpty()){
+            while(node!=null){
+                result.add(node.val);
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop().right;
+
+        }
+        return result;
+    }
+
+    /**
+     * 中序遍历
+     * 当前节点不为空或者栈不为空时，进行以下循环
+     * 1.当前节点不为空，将其入栈，将当前节点赋值为当前节点的左孩子。
+     * 2.当前节点为空，则进行出栈操作。访问出栈元素，并将当前节点赋值为出栈元素的右孩子。
+     * @param root
+     * @return
+     */
+    private List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode node = root;
+        while(node!=null || !stack.isEmpty()){
+            if(node!= null){
+                stack.push(node);
+                node = node.left;
+            }else{
+                node = stack.pop();
+                result.add(node.val);
+                node = node.right;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 后序遍历
+     * 算法流程：
+     * 当前节点不为空或者栈不为空时，进行以下循环
+     * 1.当前节点不为空，将其入栈。将当前节点赋值为当前节点的左孩子。
+     * 2.当前节点不为空，查看栈顶元素。
+     * 3.如果栈顶元素的右子树为空或者右子树是上一个访问的节点（说明当前循环的上一步是访问栈顶元素的右子树），则直接出栈。访问出栈元素，并将其赋值为上一个访问的节点。将当前节点赋值为null。
+     * 4.如果栈顶元素的右子树不为空且右子树是不是上一个访问的节点（说明当前循环的上一步是访问栈顶元素的左子树），则将当前节点赋值为栈顶元素的右子树。
+     *
+     * 算法解析：
+     * 对于前序遍历以及中序遍历，需要将父节点存入栈中，准备后续出栈访问（用于访问节点或者父节点的右子树），出栈操作肯定是在左子树处理之后再进行的。
+     * 但是对于后序遍历，由于最后访问父节点。所以从左子树返回时，不能执行出栈操作。因为右子树还需要依靠栈访问父节点元素。所以栈顶元素操作需要判断上一步是从左子树返回的还是从右子树返回的。
+     * 由于后序遍历父节点访问是在右子树之后，所以判断栈顶元素的右子树是否为上一个访问的节点，即可以知道上一步是否从右子树返回。
+     * 如果是从右子树返回，则直接出栈，访问元素。将出栈元素赋值为上一个访问的节点。
+     * 如果是从左子树返回，则不出栈，对栈顶元素的右子树进行处理。
+     * @param root
+     * @return
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+        TreeNode node = root,lastVisitNode = null;
+
+        while(node!=null || !stack.isEmpty()){
+            while(node!=null){
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.peek();
+            if(node.right == null || node.right==lastVisitNode){
+                //如果右子树为空或者刚刚访问过，则进行出栈，访问该节点，并将其赋值为上一个访问的节点。
+                node = stack.pop();
+                result.add(node.val);
+                lastVisitNode = node;
+                node = null;//下一循环直接跳到peek（）进行检查
+            }else{
+                //如果右子树不为空，且不是上一个访问的节点，则处理右子树。
+                node = node.right;
+            }
+
+        }
+        return result;
+
+    }
+
+}
--- src/main/java/com/pzx/ddsa/LargestRectangleInHistogram.java
+++ src/main/java/com/pzx/ddsa/LargestRectangleInHistogram.java
@@ -5,11 +5,12 @@ import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * 直方图中的最大矩形
- * 输入[2,1,5,6,2,3] 输出10
+ * 输入[2,1,5,6,2,3]  输出10
  */
 public class LargestRectangleInHistogram {
 
     /**
+     * 单调栈方法
      *算法过程：
      * 1.创建边界数组，表示每一个高度的左右边界（左右边小于当前高度的最近位置）
      * 1.在栈中插入单元（-1，-1），分别表示高度和索引值
@@ -55,6 +56,28 @@ public class LargestRectangleInHistogram {
     }
 
     /**
+     * 简化的单调栈方法
+     * @param heights
+     * @return
+     */
+    private static int largestRectangleArea2(int[] heights) {
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(-1);
+        int maxarea = 0;
+        for (int i = 0; i < heights.length; ++i) {
+            while (stack.peek() != -1 && heights[stack.peek()] >= heights[i])
+                maxarea = Math.max(maxarea, heights[stack.pop()] * (i - stack.peek() - 1));
+            stack.push(i);
+        }
+        while (stack.peek() != -1)
+            maxarea = Math.max(maxarea, heights[stack.pop()] * (heights.length - stack.peek() -1));
+        return maxarea;
+
+
+    }
+
+
+    /**
      * 动态规划方法
      * 1.设置二维数组maxRectangle[i][j]，其表示从i到j的最大矩形
      * 2.maxRectangle[i][j] = max（maxRectangle[i][j-1]，maxRectangle[i+1][j]，（j-i-1）*（i到j中间的最小高度））
@@ -63,7 +86,7 @@ public class LargestRectangleInHistogram {
      * @param heights
      * @return
      */
-    private static int largestRectangleArea2(int[] heights) {
+    private static int largestRectangleArea3(int[] heights) {
         if(heights.length==0)
             return 0;
 
@@ -86,13 +109,55 @@ public class LargestRectangleInHistogram {
         return maxRectangle[0][heights.length-1];
     }
 
+    /**
+     * 二维0、1矩阵求由1组成的最大矩形
+     * Input:
+     * [
+     *   ["1","0","1","0","0"],
+     *   ["1","0","1","1","1"],
+     *   ["1","1","1","1","1"],
+     *   ["1","0","0","1","0"]
+     * ]
+     *Output: 6
+     *
+     * 算法过程：
+     * 1.构建一维数组，长度为矩阵的横向长度
+     * 2.遍历二维矩阵的每一行，对行中的每一个元素，如果为0，将一维数组中的对应位置赋值为0，如果为1，则将一维数组中对应位置的数值+1.
+     * 3.一行遍历结束之后，对一维数组调用上述求直方图中最大矩形的函数。
+     * 4.比较每一行的结果，最大的即为所求结果。
+     *
+     * 算法解析：
+     * 对于每一行，将此行及以上的行当作直方图，如果此行的某个元素为0，则该位置的直方图高度为0，否则在之前的高度上+1.即将二维问题转化为一维问题。
+     *
+     * @param matrix
+     * @return
+     */
+    public int maximalRectangle(char[][] matrix) {
+        if(matrix.length == 0)
+            return 0;
+
+        int max = 0;
+        int[] height = new int[matrix[0].length];
+        for(int i = 0;i<matrix.length;i++){
+            for(int j = 0; j<height.length; j++){
+                height[j] = matrix[i][j]=='0'?0:height[j]+1;
+            }
+            max = Math.max(max,largestRectangleArea2(height));
+        }
+
+        return max;
+    }
+
+
+
+
     public static void main(String[] args) {
         int[] input = new int[1000];
         for(int j =0 ; j <10000; j++){
             for (int i = 0; i<1000; i++){
                 input[i] = Math.abs(ThreadLocalRandom.current().nextInt(1000));
             }
-            if(largestRectangleArea1(input)!=largestRectangleArea2(input)){
+            if(largestRectangleArea1(input)!=largestRectangleArea3(input)){
                 System.out.println("false");
                 break;
             }
